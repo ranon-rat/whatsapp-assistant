169d5a21e877fcd7fa6e57ad0a3a462aa0cbeb96
Replit user <>
2023-11-18 12:00:58 -0600

    Update index.ts

--- app/database/index.ts
+++ app/database/index.ts
@@ -2,29 +2,22 @@ import { messageApi, messagesFlowise, archiveProfile, limitConversation } from "
 import { Pool} from "pg"
 
 const connectionString = process.env.URI 
-
-async function connectToDB(): Promise<Pool>{
-    const client = new Pool({
-        connectionString,
-        ssl:true
-    })
-    await client.connect()
-    return client
-}
-
+const db = new Pool({
+    connectionString,
+    ssl:true
+});
+(async()=>
+await db.connect()
+)()
 export async function AddConversation(from: string, message: string, response: string) {
-    const db =await  connectToDB()
     const querycmd = `INSERT INTO history(fromNumber,message,response) VALUES ($1,$2,$3)`
    await  db.query(querycmd, [from, message, response])
-    await db.end()
 }
 export async function GetConversations(from: string): Promise<messagesFlowise[]> {
 
     const querycmd = "SELECT (message ,response) FROM history WHERE fromnumber=$1 ORDER BY ID ASC LIMIT $2"
-    const db = await connectToDB()
     let rows = await db.query(querycmd,[from,limitConversation]).then(result => result.rows) as messageApi[]
 
-    await db.end()
     return (rows.length > 0 ? rows.map(
         (r: messageApi): messagesFlowise[] => [
             { type: "userMessage", message: r.message || "error" },
@@ -35,14 +28,11 @@ export async function GetConversations(from: string): Promise<messagesFlowise[]>
 
 
 export async function checkExistence(whatsappID: string): Promise<boolean> {
-    const db = await connectToDB()
     const rows = await db.query("SELECT count(*) FROM archives where WhatsappID=$1", [whatsappID]).then(res => res.rows)
-    await db.end()
     return Object.values(rows[0])[0] as number > 0
 }
 export async function AddOrUpdateArchive(archive: archiveProfile) {
-    const db = await connectToDB(),
-        whatsappID = archive.whatsappid,
+const        whatsappID = archive.whatsappid,
         name = archive.name,
         company = archive.company,
         mbti = archive.mbti
@@ -68,15 +58,12 @@ export async function AddOrUpdateArchive(archive: archiveProfile) {
         where whatsappID=$2`, [v.value, whatsappID])
     }
 
-    await db.end()
-}
+    }
 export async function GetArchives(query: string): Promise<archiveProfile[]> {
-    const db =await  connectToDB();
     let rows: archiveProfile[] = (await db.query(`SELECT * FROM archives
         WHERE name LIKE $1 
           OR company LIKE $1 
           OR mbti LIKE $1
           OR whatsappid LIKE $1`, [`%${query||""}%`])).rows
-    await db.end()
     return rows
 }
