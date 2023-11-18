2a33ca4d6e66fd58bf3cff112284dcd68fd40b5d
Replit user <>
2023-11-18 00:55:51 -0600

    pues parece ya funcionar todo bien

--- app/parts/index.tsx
+++ app/parts/index.tsx
@@ -45,65 +45,57 @@ export function search(token: string) {
 
     return <div id="search">
 
-            <form id="search-form" action={"/?token=" + token}>
-                <label htmlFor="default-search"
-                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <form id="search-form" action={"/?token=" + token}>
+            <label htmlFor="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
 
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" id="default-search"
-                        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search for name, company, number or MBTI"
-                        name="search"
-                        required
-                    />
-                    <input className="hidden" type="text" name="token" defaultValue={token} />
-                    <button type="submit"
-                        className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
                 </div>
-            </form>
+                <input type="search" id="default-search"
+                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search for name, company, number or MBTI"
+                    name="search"
+                    required
+                />
+                <input className="hidden" type="text" name="token" defaultValue={token} />
+                <button type="submit"
+                    className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+        </form>
 
-        </div>
-    
+    </div>
+
 }
 function row(v: archiveProfile, index: number) {
-    return <div>
-        <Fragment key={index}>
-
-            <tr >
-
+    return <Fragment key={index}>
+            <tr>
                 <td
                     className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                    <div>{v.whatsappid}</div>
-                    </td>
+                    {v.whatsappid}</td>
                 <td
                     className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-                    {v.name}
-                    </td>
+                    {v.name}</td>
                 <td
                     className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-                    {v.company}
-                    </td>
+                    {v.company}</td>
 
                 <td
                     className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-                    {v.mbti}
-                    </td>
+                    {v.mbti}</td>
 
             </tr>
         </Fragment>
-        </div>
-    
+
 }
 export async function table(query: string) {
     const rows = (await GetArchives(query)).map(row)
-    if (rows.length == 0&& query!=="") {
+    if (rows.length == 0 && query !== "") {
         return <div id="nothing-found">
             <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
                 <p className="font-bold">404 Nothing was found</p>
@@ -111,9 +103,9 @@ export async function table(query: string) {
             </div>
         </div>
     }
-    if(rows.length==0){
+    if (rows.length == 0) {
         return <div id="nothing-found">
-            <br/>
+            <br />
             <h1 className="text-white">
                 Empty database
             </h1>
@@ -140,7 +132,7 @@ export async function table(query: string) {
                         </tr>
                     </thead>
                     <tbody className="bg-white dark:bg-slate-800" id="records">
-                       <div> {rows}</div>
+                         {rows}
 
                     </tbody>
                 </table>
