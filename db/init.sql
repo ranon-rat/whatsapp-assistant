a2757ed5ff498a73fcd7a1aad1f073e65653233a
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-11-26 23:57:21 -0600

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE history(
     ID SERIAL PRIMARY KEY NOT NULL,
     fromNumber VARCHAR(30),
@@ -16,4 +15,12 @@ CREATE TABLE archives(
   Solution TEXT,
   Thoughts,
   Memories TEXT
+);
+CREATE TABLE Analysis(
+	ID SERIAL PRIMARY KEY NOT NULL,
+	WhatsappID TEXT,
+	Memories TEXT,
+	Thoughts TEXT,
+	Problem TEXT,
+	Solution TEXT
 );
